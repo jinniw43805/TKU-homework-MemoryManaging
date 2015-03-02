fd482cba5df161157daab81dacbe4bec27a2cb7c
Tony <jinniw43805@gmail.com>
2015-03-02 23:35:23 +0800

    Initial Commit

---
+++ GitPractice/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  GitPractice
+//
+//  Created by 郭彥君 on 15/3/2.
+//  Copyright (c) 2015年 郭彥君. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
