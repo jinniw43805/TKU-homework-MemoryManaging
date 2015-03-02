7fe539a21de6db896433613a2a8512372f553529
Tony <jinniw43805@gmail.com>
2015-03-02 23:40:52 +0800

    for teacher example test

--- GitPractice/main.c
+++ GitPractice/main.c
@@ -7,13 +7,38 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define COUNT 10000
+#define TIME_LIMIT 1000
+#define MEM_LIMIT 500
+#define TIME_OFFSET 10
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
-    printf("hello 2 world\n");
+    FILE *fp;
+    int cur_time=2, i;
+    int p_id = 1000;
+    
+    
+    srand((int)time(NULL));
+    fp = fopen("input.txt", "w");
+    
+    for (i=0; i< COUNT; i++)
+    {
+        fprintf(fp, "%d ", p_id);
+        p_id++;
+        
+        fprintf(fp, "%d ", cur_time);
+        cur_time = cur_time + 1+ (rand()%TIME_OFFSET);
+        
+        fprintf(fp, "%d ", 1+ (rand()%TIME_LIMIT));
+        fprintf(fp, "%d\n", 1+ (rand()%MEM_LIMIT));
+        
+    }
+    
+    fclose(fp);
     return 0;
 }
 
