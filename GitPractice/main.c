00e15e13561e5b6c7cfbca157791fb61e4bca1d9
Tony <jinniw43805@gmail.com>
2015-03-06 15:38:33 +0800

    add schedule event queue!!!

--- GitPractice/main.c
+++ GitPractice/main.c
@@ -23,10 +23,16 @@ typedef struct node{
 typedef struct schedule_node{
     int processID;
     int time;
+    int alloSpace;
+    enum states{allocate,release}States;
+    struct schedule_node *next;
+    
+    //for allocate node
+    struct schedule_node *releaseNode;
+    
+    //for release node
     int startAd;
     int endAd;
-    enum state{allocate,release};
-    struct schedule_node *next;
 }Schedule_node;
 
 typedef struct queue{
@@ -51,15 +57,17 @@ int main(int argc, const char * argv[])
     char *tok=" ";
     char *parseTmpString;
     
-    Queue q;
-    q.head=NULL;
-    q.tail=NULL;
-    Schedule_queue Scheq;
-    Scheq.head=NULL;
-    Scheq.tail=NULL;
+    Queue *q;
+    q=(Queue*)malloc(sizeof(Queue));
+    q->head=NULL;
+    q->tail=NULL;
+    Schedule_queue *Scheq;
+    Scheq=(Schedule_queue*)malloc(sizeof(Schedule_queue));
+    Scheq->head=NULL;
+    Scheq->tail=NULL;
+    
     
     Node *ptr;
-    Node *ptrSchedule;
     FILE *fp;
     
     int i,t,globalEndTime=0;
@@ -106,16 +114,16 @@ int main(int argc, const char * argv[])
             globalEndTime=newnode->endTime;
         }
         //End calculate
-     //   printf("%d %d %d %d",newnode->processID,newnode->startAd,newnode->processTime,newnode->alloSpace);
+        //   printf("%d %d %d %d",newnode->processID,newnode->startAd,newnode->processTime,newnode->alloSpace);
         
         //為第一個節點
-        if (q.head == NULL) {
-            q.head=newnode;
-            q.tail=newnode;
+        if (q->head == NULL) {
+            q->head=newnode;
+            q->tail=newnode;
         }else{//為最後一個節點
-            q.tail->next=newnode;
-            q.tail=newnode;
-            q.tail->next=NULL;
+            q->tail->next=newnode;
+            q->tail=newnode;
+            q->tail->next=NULL;
         }
         
         
@@ -125,23 +133,34 @@ int main(int argc, const char * argv[])
     
     
     //End read
-
+    
     //enqueue
-    enSchedule_queue(&q, &Scheq);
+    
+    enSchedule_queue(q, Scheq);
     //End enqueue
+    Schedule_node *ptrSchedule;
+    ptrSchedule=Scheq->head;
+    while (ptrSchedule!=NULL) {
+        if (ptrSchedule->States==allocate) {
+            printf("Enter process ID: %d , Enter time: %d\n",ptrSchedule->processID,ptrSchedule->time);
+        }else if (ptrSchedule->States==release){
+            printf("Release process ID: %d ,Release time: %d\n",ptrSchedule->processID,ptrSchedule->time);
+        }
+        ptrSchedule=ptrSchedule->next;
+    }
     //Test read whether success
-//    printf("processID startTime processTime alloSpace endTime\n");
-//    ptr=q.head;
-//    int count=0;
-//    while (ptr!=NULL) {
-//       
-//           printf("%d %d %d %d %d",ptr->processID,ptr->startTime,ptr->processTime,ptr->alloSpace,ptr->endTime);
-//        printf("\n");
-//        ptr=ptr->next;
-//    }
-//    printf("System end time: %d \n",globalEndTime);
-//    
-    ptr=q.head;
+    //    printf("processID startTime processTime alloSpace endTime\n");
+    //    ptr=q.head;
+    //    int count=0;
+    //    while (ptr!=NULL) {
+    //
+    //           printf("%d %d %d %d %d",ptr->processID,ptr->startTime,ptr->processTime,ptr->alloSpace,ptr->endTime);
+    //        printf("\n");
+    //        ptr=ptr->next;
+    //    }
+    //    printf("System end time: %d \n",globalEndTime);
+    //
+    ptr=q->head;
     //Test end
     
     //First-Allocation Algo
@@ -163,8 +182,8 @@ int main(int argc, const char * argv[])
     
     //printfBuffer
     
-
-   // printBufferStatus();
+    
+    // printBufferStatus();
     
     //end printfBuffer
     
@@ -173,23 +192,80 @@ int main(int argc, const char * argv[])
 }
 void enSchedule_queue(Queue *queue,Schedule_queue *Scheq){
     Node *ptr;
-    Schedule_node *new_sche_node_allo;
-    Schedule_node *new_sche_node_relea;
+    Schedule_node *ptrSchedule;
+    
     ptr=queue->head;
     while (ptr!=NULL) {
+        Schedule_node *new_sche_node_allo;
+        Schedule_node *new_sche_node_relea;
         //Test for queue
-//        printf("%d %d %d %d %d",ptr->processID,ptr->startTime,ptr->processTime,ptr->alloSpace,ptr->endTime);
-//        printf("\n");
+        printf("%d %d %d %d %d",ptr->processID,ptr->startTime,ptr->processTime,ptr->alloSpace,ptr->endTime);
+        printf("\n");
         //End test
-        new_sche_node_allo=(Schedule_node*)malloc(sizeof(Schedule_node));
         
+        
+        //allocate event
+        new_sche_node_allo=(Schedule_node*)malloc(sizeof(Schedule_node));
         new_sche_node_allo->processID=ptr->processID;
         new_sche_node_allo->time=ptr->startTime;
+        new_sche_node_allo->alloSpace=ptr->alloSpace;
+        new_sche_node_allo->States=allocate;
+        
+        //release event
         
         new_sche_node_relea=(Schedule_node*)malloc(sizeof(Schedule_node));
-        
         new_sche_node_relea->processID=ptr->processID;
         new_sche_node_relea->time=ptr->endTime;
+        new_sche_node_relea->alloSpace=ptr->alloSpace;
+        new_sche_node_relea->States=release;
+        
+        
+        //binding allocate node with release node
+        new_sche_node_allo->releaseNode=new_sche_node_allo;
+        //visit the schedule queue
+        
+        //if first event
+        if (Scheq->head==NULL && Scheq->tail==NULL) {
+            Scheq->head=new_sche_node_allo;
+            Scheq->tail=new_sche_node_relea;
+            new_sche_node_allo->next=new_sche_node_relea;
+            new_sche_node_relea->next=NULL;
+        }else{
+            
+            
+            //for allocate
+            ptrSchedule=Scheq->head;
+            while (ptrSchedule!=NULL) {
+                if (new_sche_node_allo->time > Scheq->tail->time) {
+                    Scheq->tail->next=new_sche_node_allo;
+                    Scheq->tail=new_sche_node_allo;
+                }else if (new_sche_node_allo->time < Scheq->head->time){
+                    new_sche_node_allo->next=Scheq->head;
+                    Scheq->head=new_sche_node_allo;
+                }else if (new_sche_node_allo->time > ptrSchedule->time &&
+                          new_sche_node_allo->time < ptrSchedule->next->time){
+                    new_sche_node_allo->next=ptrSchedule->next;
+                    ptrSchedule->next=new_sche_node_allo;
+                }
+                ptrSchedule=ptrSchedule->next;
+            }
+            //for release
+            ptrSchedule=Scheq->head;
+            while (ptrSchedule!=NULL) {
+                if (new_sche_node_relea->time > Scheq->tail->time) {
+                    Scheq->tail->next=new_sche_node_relea;
+                    Scheq->tail=new_sche_node_relea;
+                }else if (new_sche_node_relea->time < Scheq->head->time){
+                    new_sche_node_relea->next=Scheq->head;
+                    Scheq->head=new_sche_node_relea;
+                }else if (new_sche_node_relea->time > ptrSchedule->time &&
+                          new_sche_node_relea->time < ptrSchedule->next->time){
+                    new_sche_node_relea->next=ptrSchedule->next;
+                    ptrSchedule->next=new_sche_node_relea;
+                }
+                ptrSchedule=ptrSchedule->next;
+            }
+        }
         ptr=ptr->next;
     }
 }
@@ -200,7 +276,7 @@ void printBufferStatus(void){
     {
         printf("%c ",buffer[i]);
     }
-
+    
 }
 void initBufferStatus(void){
     int i;
